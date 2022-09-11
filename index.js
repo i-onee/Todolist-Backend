c5842b344dee181a94912c199c707d60daf5af19
i-one7 <ridhoang93@gmail.com>
2022-09-12 00:40:40 +0700

    add index for root files

---
+++ index.js
@@ -0,0 +1,30 @@
+import router from './routers/activity.router.js';
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const config = {
+	compass 	: process.env.MONGODB_COMPASS,
+	atlas 	: process.env.MONGODB_ATLAS,
+	dbase		: process.env.DB_NAME,
+	port		: process.env.PORT
+};
+
+mongoose.connect(`${config.atlas || config.compass}/${config.dbase}`, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+});
+
+const dbase = mongoose.connection;
+dbase.once('open', () => console.info('mongoose connected!!'));
+dbase.on('error', error => console.error(error));
+
+const app = express();
+
+app.listen(config.port || 5000, () => console.info('server running at port 5000'));
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
