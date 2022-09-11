7dce84c7bc581b85e17c84624dd63be63c75692d
i-one7 <ridhoang93@gmail.com>
2022-09-12 00:02:08 +0700

    add router for accessing api to database

---
+++ routers/activity.router.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+	getActivityByID,
+	updateActivity,
+	deleteActivity,
+	createActivity,
+	getActivity
+} from '../controllers/activity.control';
+
+const router = express.Router();
+
+router.delete('/activity/:id', deleteActivity);
+router.patch('/activity/:id', updateActivity);
+router.get('/activity/:id', getActivityByID);
+router.post('/activity', createActivity);
+router.get('/activity', getActivity);
+
+export default router;
